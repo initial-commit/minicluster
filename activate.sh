f91a3560f5f5548edf6137f18db5bcaa4bfc9a8e
Flavius Aspra <flavius.as@gmail.com>
2023-09-10 19:08:37 +0200

    Adapt temporary script to activate minicluster inside a VM

--- activate.sh
+++ activate.sh
@@ -2,7 +2,8 @@ export PATH=$(pwd)/bin/commands:$PATH
 stty rows 46 cols 189
 # turn on line wrapping
 printf %b '\033[?7h'
-bootstrap-host.sh
-pacman -Qi vim &>/dev/null || {
-	pacman -S --noconfirm vim tmux tree htop dnsmasq dmidecode wget ncdu 
-}
+reset
+#bootstrap-host.sh
+#pacman -Qi vim &>/dev/null || {
+#	pacman -S --noconfirm vim tmux tree htop dnsmasq dmidecode wget ncdu
+#}
